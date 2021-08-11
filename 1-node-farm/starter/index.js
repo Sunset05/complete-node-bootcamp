4358d92ee8666f09b855c9166b0b0d1071e45969
Keith Funk <Keith.C.Funk@gmail.com>
2021-08-11 13:33:45 -0600

    using the responses from async functions to write and read different files, and combine outputs.

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,7 +1,24 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
+//Blocking, synchronous code
+// const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
+// const textOuput = `This is what we know about the avocado: ${textInput}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOuput);
+// console.log("File written!");
 
-const textOuput = `This is what we know about the avocado: ${textInput}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOuput);
-console.log('File written!');
+// Non-clocking, asynchronous way
+fs.readFile("./txt/start.txt", "utf-8", (error, data1) => {
+    if(err) return console.log("ERROR")
+
+    fs.readFile(`./txt/${data1}.txt`, "utf-8", (error, data2) => {
+        console.log(data2);
+        fs.readFile("./txt/append.txt", "utf-8", (error, data3) => {
+            console.log(data3);
+
+            fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('Your file has been written ')
+            })
+        });
+    });
+});
+console.log("will read file!");
