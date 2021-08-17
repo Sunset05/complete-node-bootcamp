d29b3dd9a2e3a3aa5145cf812644f15b809cf2dd
Keith Funk <Keith.C.Funk@gmail.com>
2021-08-17 11:21:22 -0600

    sending json to front end

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (request, response) => {
-  response.status(200).send({'Hello', app: 'Natours'});
+app.get('/', (_, response) => {
+  response.status(200).json({ message: 'Hello from server', app: 'Natours' });
 });
 
 app.listen(port, () => {
